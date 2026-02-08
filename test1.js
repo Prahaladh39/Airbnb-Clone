fe5a1c0910e627621a2efc8ee3b942eaaadcf89a
Prahaladh39 <prahaladh05@gmail.com>
2026-02-08 18:03:13 +0530

    Refactor add function for student data processing

--- test1.js
+++ test1.js
@@ -1,25 +1,42 @@
 function add(a, b) {
-  if (!order || !order.items || order.items.length === 0) {
-    return { success: false, message: "Invalid order" };
+    if (!student || !student.marks || student.marks.length === 0) {
+    return { success: false, message: "Invalid student data" };
   }
 
   let total = 0;
+  let failedSubjects = 0;
 
-  for (let i = 0; i < order.items.length; i++) {
-    if (order.items[i].price <= 0 || order.items[i].qty <= 0) {
-      return { success: false, message: "Invalid item data" };
+  for (let i = 0; i < student.marks.length; i++) {
+    let mark = student.marks[i];
+
+    if (mark < 0 || mark > 100) {
+      return { success: false, message: "Invalid marks detected" };
     }
-    total += order.items[i].price * order.items[i].qty;
+
+    total += mark;
+
+    if (mark < 35) {
+      failedSubjects++;
+    }
   }
 
-  let discount = total > 1000 ? total * 0.1 : 0;
-  let finalAmount = total - discount;
+  let average = total / student.marks.length;
+  let status = failedSubjects > 0 ? "FAIL" : "PASS";
+  let grade;
+
+  if (average >= 85) grade = "A";
+  else if (average >= 70) grade = "B";
+  else if (average >= 55) grade = "C";
+  else grade = "D";
 
   return {
     success: true,
-    itemsCount: order.items.length,
-    totalAmount: total,
-    discountApplied: discount,
-    payableAmount: finalAmount
+    totalMarks: total,
+    averageMarks: average,
+    grade: grade,
+    status: status,
+    failedSubjects: failedSubjects
   };
+
+
 }
